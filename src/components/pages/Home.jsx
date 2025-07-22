835ba3fe7e87b8e407a9784d94928c2fdbe90af5
Manuel Aguirre <aguirreman98@gmail.com>
2025-07-22 18:51:40 -0500

    Working on the Home component

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='relative h-screen w-full'>
+      <img src="/assets/hero-image.jpeg" alt="Hero"
+        className='absolute inset-0 w-full h-full object-cover z-0' />
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center">
+        <h1 className="text-4xl md:text-6xl font-bold">Manuel Aguirre</h1>
+        <p className="text-xl mt-4 font-medium">Aspiring Front-End Developer</p>
+      </div>
+    </div>
+  )
+}
