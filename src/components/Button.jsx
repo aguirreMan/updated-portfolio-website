3cf1ba56063017180d51c7cabfeaf84929106e71
Manuel Aguirre <aguirreman98@gmail.com>
2025-08-06 18:58:12 -0500

    Worked on adding framer motion to my home page website and the button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 export default function Button() {
   return (
     <Link to='/Contact'>
-    <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded cursor-pointer">
-      Button
-    </button>
+    <motion.button className="bg-gray-400 text-white font-bold py-2 px-4 border-b-4 border-b-gray-600 rounded cursor-pointer"
+    initial={{ opacity: 0.8, y: 20}}
+    animate={{ opacity: 0.88, y: 0}}
+    whileHover={{scale: 1.1}}
+    whileTap={{ scale: 0.95}}
+    transition={{ type: 'tween', ease: 'easeOut', duration: 0.8}}
+    >
+      Lets have a cup of Coffee and talk! 
+    </motion.button>
     </Link >
   )
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import Button from '../Button'
+import { motion } from 'framer-motion'
 
 export default function Home() {
+  const name = 'Manuel Aguirre'
   return (
     <div className='relative h-screen w-full'>
       <img src="/assets/hero-image.jpeg" alt="Hero"
         className='absolute inset-0 w-full h-full object-cover z-0' />
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center">
-        <h1 className="text-4xl md:text-6xl font-bold">Manuel Aguirre</h1>
-        <p className="text-xl mt-4 font-medium pb-5">Aspiring Front-End Developer</p>
+        <h1 className="text-4xl md:text-6xl font-bold text-neutral-500 tracking-wider">
+          {name.split('').map((char, index) => (
+            <motion.span
+              key={index}
+              initial={{ opacity: 0, x: -18 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 2.5, delay: index * 0.2 }} >
+              {char}
+            </motion.span>
+          ))}
+        </h1>
+        <p className="text-2xl mt-4 font-medium pb-6 tracking-wide">Aspiring Front-End Developer</p>
         <Button />
       </div>
     </div>
