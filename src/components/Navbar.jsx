09ef3f66fa4b43ea268ee67ee905d0e06bcf0c8c
Manuel Aguirre <aguirreman98@gmail.com>
2025-09-04 15:24:42 -0500

    Today worked on adding a route Link to my home page and worked on some styling in my projects page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import NavLinks from './NavLinks'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { IoMdClose } from 'react-icons/io'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
@@ -11,7 +12,9 @@ export default function Navbar() {
     <>
       {/* HEADER */}
       <header className='w-full absolute top-0 left-0 flex justify-between items-center p-4 bg-transparent z-20'>
-        <h1 className='text-3xl text-custom-text-color font-bold cursor-pointer'>MA</h1>
+        <h1 className='text-3xl text-custom-text-color font-bold cursor-pointer'>
+          <Link to='/'>MA</Link>
+        </h1>
         <div className='hidden md:flex gap-4'>
           <NavLinks />
         </div>
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -8,9 +8,9 @@ export default function Projects() {
         { title: 'Dashboard', description: 'Built with React typescript tailwind' }
     ]
     return (
-        <div className='backdrop-grayscale-50 mt-20'>
-            <h1 className='text-3xl text-custom-text-color mt-10 flex justify-center pb-10'>My Projects</h1>
-            <div className='flex justify-center gap-8'>
+        <div className='bg-custom-background-color min-h-screen flex flex-col items-center pt-10'>
+            <h1 className='text-3xl text-custom-text-color pb-10 mt-20'>My Projects</h1>
+            <div className='flex justify-center gap-8 flex-wrap'>
                 {projectData.map((project, index) => {
                     return <ProjectCard key={index} title={project.title} description={project.description} />
                 })}
