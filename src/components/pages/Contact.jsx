eb1c901304fddaca37e7ffc29fcbac6fc05d2e82
Manuel Aguirre <aguirreman98@gmail.com>
2026-02-22 01:56:15 -0600

    Updated my form for Netlify

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Coffee } from 'lucide-react'
 import { FieldSet } from '../ui/field'
 import { Label } from '../ui/label'
@@ -6,10 +7,52 @@ import { Textarea } from '../ui/textarea'
 import { Button } from '../ui/button'
 
 export default function Contact() {
+  const [formSubmit, setFormSubmit] = useState(false)
+  const [formFields, setFormFields] = useState({ name: '', email: '', message: '' })
+
+  function formChange(event) {
+    setFields(prev => ({ ...prev, [event.target.name]: event.target.value}))
+  }
+
+  function encode(data) {
+      return Object.keys(data)
+        .map(
+          (key) =>
+            encodeURIComponent(key) +
+            '=' +
+            encodeURIComponent(data[key])
+        )
+        .join('&')
+    }
+
+  async function submitForm(event) {
+    event.preventDefault()
+    await fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...formFields }),
+    }),
+    setFormSubmit(true)
+  }
+
+  if (formSubmit) {
+    return (
+      <div className="flex flex-col items-center min-h-screen px-24
+        justify-center bg-background"
+      >
+        <h1 className="flex flex-row gap-2 text-4xl text-foreground">
+          Lets grab a cup of{' '}
+          <Coffee className="w-10 h-10 text-muted-foreground" strokeWidth={2.5} />
+        </h1>
+        <p className="text-xl text-muted-foreground">Thank you for your message!</p>
+      </div>
+    )
+  }
+
   return (
     <div
-      className="flex flex-col items-center min-h-screen px-24 
-        justify-center bg-background px-8"
+      className="flex flex-col items-center min-h-screen px-24
+        justify-center bg-background"
     >
       <h1 className="flex flex-row gap-2 text-4xl text-foreground">
         Lets grab a cup of{' '}
@@ -20,6 +63,7 @@ export default function Contact() {
         method="POST"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
+        onSubmit={submitForm}
         className="w-full max-w-md mb-8 flex flex-col gap-4 pt-4"
       >
         <Input type="hidden" name="form-name" value="contact" />
@@ -31,6 +75,8 @@ export default function Contact() {
             name="name"
             type="text"
             required
+            value={formFields.name}
+            onChange={formChange}
             className=" border transition-all duration-200"
           />
         </FieldSet>
@@ -40,6 +86,8 @@ export default function Contact() {
             name="email"
             type="email"
             required
+            value={formFields.email}
+            onChange={formChange}
             className="transition-all duration-200"
           />
         </FieldSet>
@@ -51,6 +99,8 @@ export default function Contact() {
             name="message"
             placeholder="Send message here"
             required
+            value={formFields.message}
+            onChange={formChange}
             className="transition-all duration-200 leading-relaxed"
           />
         </FieldSet>
