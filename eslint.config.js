4d1c2e1b2ef1a0ffd6d9dd8dd2462849ba18d90c
Manuel Aguirre <aguirreman98@gmail.com>
2026-07-26 00:34:19 -0500

    Added storybook to my portfolio and wrote stories for button component

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,27 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js'
 import globals from 'globals'
 import pluginReact from 'eslint-plugin-react'
 import pluginReactHooks from 'eslint-plugin-react-hooks'
 import { defineConfig } from 'eslint/config'
 
-export default defineConfig([
-  js.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    files: ['**/*.{js,jsx}'],
-    plugins: {
-      'react-hooks': pluginReactHooks,
-    },
-    rules: {
-      ...pluginReactHooks.configs.recommended.rules,
-      semi: ['error', 'never'],
-      quotes: [
-        'error',
-        'single',
-        { avoidEscape: true, allowTemplateLiterals: true },
-      ],
-      'no-unused-expressions': 'error',
-      'react/react-in-jsx-scope': 'off',
-    },
-    languageOptions: { globals: globals.browser },
+export default defineConfig([js.configs.recommended, pluginReact.configs.flat.recommended, {
+  files: ['**/*.{js,jsx}'],
+  plugins: {
+    'react-hooks': pluginReactHooks,
   },
-])
+  rules: {
+    ...pluginReactHooks.configs.recommended.rules,
+    semi: ['error', 'never'],
+    quotes: [
+      'error',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: true },
+    ],
+    'no-unused-expressions': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+  languageOptions: { globals: globals.browser },
+}, ...storybook.configs["flat/recommended"]])
